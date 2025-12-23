59eda83c7df5bac4ef8e3fedbe2235b2d7dad7b4
ManojLL <manojlakshan421@gmail.com>
2025-12-23 22:38:58 +0530

    added backend service

---
+++ TiNyUrlService/src/test/java/com/manojll/TiNyUrlService/TinyUrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manojll.TiNyUrlService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyUrlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
